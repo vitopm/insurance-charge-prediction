d6311844ab41e1aea421ddd5c0796cddc060b2bc
Vito <vito.pramudita1502@gmail.com>
2022-05-10 00:03:40 +0700

    Added a new page

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from matplotlib.pyplot import show
 import streamlit as st
 from predict_page import show_predict_page
+from explore_page import show_explore_page
 
-st.sidebar.selectbox("Explore or Predict", ("Predict", "Explore Page"))
-
-show_predict_page()
+page = st.sidebar.selectbox("Explore or Predict", ("Predict", "Explore Page"))
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
--- explore_page.py
+++ explore_page.py
@@ -1,3 +1,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+def show_explore_page():
+    st.header("Data exploratory")
+    st.subheader("Nothing is written here yet, hang on for a moment!")
